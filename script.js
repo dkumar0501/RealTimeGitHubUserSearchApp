10822173158ec7f96288741c9ded0f164edf4a95
D Kumar <deepkumar0501@gmail.com>
2025-07-16 22:04:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchInput = document.getElementById("search");
-const searchBtn = document.getElementById("search-btn");
+const searchBtn = document.getElementById("search-btn"); 
 const profileContainer = document.getElementById("profile-container");
 const errorContainer = document.getElementById("error-container");
 const avatar = document.getElementById("avatar");
